66dd9189ac6c3f7b43f8461760098e64928ca5c3
danludwig <danludwig@danludwig.com>
2015-12-19 11:39:23 -0500

    Rename windows folders.

--- src/commondomain/CommonDomain.Persistence/IConflictWith.cs
+++
@@ -1,7 +0,0 @@
-namespace CommonDomain.Persistence
-{
-  public interface IConflictWith
-  {
-    bool ConflictsWith(object uncommitted, object committed);
-  }
-}
--- src/commondomain/CommonDomain.Persistence/IConstructAggregates.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CommonDomain.Persistence
-{
-  public interface IConstructAggregates
-  {
-    IAggregate Build(Type type, Guid id, IMemento snapshot);
-  }
-}
