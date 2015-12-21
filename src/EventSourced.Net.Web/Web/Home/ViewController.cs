171aa35dc31607f82fb8b02cbeb0bc9edcd2aeab
danludwig <danludwig@danludwig.com>
2015-12-21 16:19:38 -0500

    Scaffold out initial user interface.

---
+++ src/EventSourced.Net.Web/Web/Home/ViewController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace EventSourced.Net.Web.Home
+{
+  public class ViewController : Controller
+  {
+    [HttpGet, Route("")]
+    public IActionResult Home() {
+      return View("~/Web/Home/Home.cshtml");
+    }
+
+    [HttpGet, Route("about")]
+    public IActionResult About() {
+      return View("~/Web/Home/About.cshtml");
+    }
+
+    [HttpGet, Route("contact")]
+    public IActionResult Contact() {
+      return View("~/Web/Home/Contact.cshtml");
+    }
+  }
+}
---
+++ src/EventSourced.Net.Web/Web/Users/Register/ViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace EventSourced.Net.Web.Users.Register
+{
+  public class ViewController : Controller
+  {
+    [HttpGet, Route("register")]
+    public IActionResult Get() {
+      return View("~/Web/Users/Register/Register.cshtml");
+    }
+  }
+}
