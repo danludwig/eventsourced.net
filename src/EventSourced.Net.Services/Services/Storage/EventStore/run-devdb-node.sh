ddbdb35193753e059a7660494d66efc9e91b23e9
Dan Ludwig <mail@danludwig.com>
2015-12-30 08:38:52 -0500

    Make mac devdb shell script use args to start eventstored.

--- src/EventSourced.Net.Services/Services/Storage/EventStore/run-devdb-node.sh
+++ src/EventSourced.Net.Services/Services/Storage/EventStore/run-devdb-node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 EVENTSTORE_DIR="{EventStoreInstallPath}"
 cd $EVENTSTORE_DIR
-LD_LIBRARY_PATH=${EVENTSTORE_DIR}:$LD_LIBRARY_PATH ${EVENTSTORE_DIR}/eventstored $@
+LD_LIBRARY_PATH=${EVENTSTORE_DIR}:$LD_LIBRARY_PATH ${EVENTSTORE_DIR}/eventstored --db ./db --log ./log --run-projections=system --start-standard-projections $@
