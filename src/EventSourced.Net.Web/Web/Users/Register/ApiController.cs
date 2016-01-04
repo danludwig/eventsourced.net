a78c12495e131dc7dfa0e2cb708e90a75f846ab3
danludwig <danludwig@danludwig.com>
2016-01-04 14:51:07 -0500

    Remove unnecessary correlation socket from registration workflow.

--- src/EventSourced.Net.Web/Web/Users/Register/ApiController.cs
+++ src/EventSourced.Net.Web/Web/Users/Register/ApiController.cs
@@ -27,9 +27,6 @@ namespace EventSourced.Net.Web.Users.Register
       await Command.SendAsync(new PrepareUserContactChallenge(correlationId, emailOrPhone))
         .ConfigureAwait(false);
 
-      WebSockets.AddCorrelationService(correlationId);
-      var correlationUrl = WebSockets.GetCorrelationUri(correlationId).ToString();
-      Response.Headers.Add("X-Correlation-Socket", correlationUrl);
       string location = Url.RouteUrl("RegisterVerifyRoute", new { correlationId });
       return new CreatedResult(location, new { CorrelationId = correlationId, });
     }
